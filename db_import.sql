e42ef2e7e49e68295b235eeec1e3c33aa54eb3cf
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-07-30 02:16:06 -0300

    Criando o banco de dados e suas tabelas

---
+++ db_import.sql
@@ -0,0 +1,19 @@
+create database lista_tarefas;
+
+use lista_tarefas;
+
+create table tb_status(
+	id int not null primary key auto_increment,
+    status varchar(25) not null
+);
+
+insert into tb_status(status)values('pendente');
+insert into tb_status(status)values('realizado');
+
+create table tb_tarefas(
+	id int not null primary key auto_increment,
+    id_status int not null default 1,
+    foreign key(id_status) references tb_status(id),
+	tarefa text not null,
+    data_cadastrado datetime not null default current_timestamp
+)
