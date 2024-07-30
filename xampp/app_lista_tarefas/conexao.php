b4988a649b57b1116e30862fdaf1278232520df9
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-07-30 02:17:33 -0300

    Arquivo que regula a conexão ao banco de dados

---
+++ xampp/app_lista_tarefas/conexao.php
@@ -0,0 +1,30 @@
+<?php
+
+    class Conexao{
+
+        private $host = 'localhost';
+        private $dbname = 'lista_tarefas';
+        private $user = 'root';
+        private $pass = '';
+
+        public function conectar(){
+
+            try{
+
+                $conexao = new PDO(
+                    "mysql:host=$this->host;dbname=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+
+                return $conexao;
+
+            } catch(PDOException $e){
+                echo '<p>'.$e->getMessage().'</p>';
+            }
+
+        }
+
+    }
+
+?>
